968bd14e3e17e07b835990fa07f536facafb0a5b
Miika Avela <h4211@student.jamk.fi>
2019-04-11 20:21:29 +0300

    Create UplinkBuffer methods
    -send for packet sending
    -addPacket for adding single packet into the buffer

--- include/hw/UplinkBuffer.hpp
+++ include/hw/UplinkBuffer.hpp
@@ -2,17 +2,21 @@
 #define SANLACLASSIC_HW_UPLINKBUFFER_H_
 
 #include "hw/MessageBuffer.hpp"
+#include "common/SanlaMessage.hpp"
+#include "common/SanlaPacket.hpp"
+#include "constants.hpp"
 
 namespace sanla {
     namespace hw_interfaces {
         namespace mq {
             class UplinkBuffer:MessageBuffer {
                 public:
-                std::vector<SanlaPacket> packetBuffer;
-                
-                bool send(SanlaPacket);
-                
+                void send();
+                bool addPacket(SanlaPacket);
                 virtual uint32_t GetBufferLength() override;
+                
+                private:
+                std::vector<SanlaPacket> packetBuffer;
             };
         }; // mq
     }; // hw_interfaces
--- src/UplinkBuffer.cpp
+++ src/UplinkBuffer.cpp
@@ -1,8 +1,44 @@
 #include "hw/UplinkBuffer.hpp"
-#include "common/SanlaMessage.hpp"
 
-using namespace sanla::hw_interfaces::mq;
+namespace sanla {
+namespace hw_interfaces {
+namespace mq {
 
-bool UplinkBuffer::send(SanlaPacket packet){
+void UplinkBuffer::send(){
+    while (!packetBuffer.empty()) {
+        /*
+        TODO: LoRaModule needs to be accessed before this can be used!
+        */
+
+        /*if (!LoRa.beginPacket()) {
+            break;
+        }
+        char serialized_packet[41]{};
+        //sanla::sanlamessage::htonSanlaPacket(packetBuffer.front(), serialized_packet);
+        LoRa.write((uint8_t*)serialized_packet, 41);
+        LoRa.endPacket();
+
+        // Revert back to listening mode
+        LoRa.receive();*/
+    }
+}
+
+/**
+ * @brief Add SanlaPacket into buffer.
+ * 
+ * @param packet SanlaPacket to be added
+ * @return true If packet was added
+ * @return false If packet wasn't added. This is caused by buffer already being full.
+ */
+bool UplinkBuffer::addPacket(SanlaPacket packet){
+    if (packetBuffer.size() < UPLINKBUFFER_MAX_SIZE) {
+        packetBuffer.push_back(packet);
+        return true;
+    }
     return false;
 }
+
+
+} // mq
+} // hw_interfaces
+} // sanla
